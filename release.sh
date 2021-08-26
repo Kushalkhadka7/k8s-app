1966a2b3934a3d6abf9bda6a915f5e5860c7fefd
test <test@test.com>
2021-08-26 22:40:48 +0545

    Update yaml file

--- release.sh
+++ release.sh
@@ -6,12 +6,14 @@ apps=$(git diff --name-only | sort -u | grep -oP "apps\/.+?\/" | cat | uniq)
 
 APP_DOCKER_REGISTRY="crkushal7"
 
-if (( ${#apps[@]} )); then
-  printf "Publishing images...\n"
+if (( ${#apps[@]} != 0 )); then
+  printf "Building docker images...\n$apps"
 
   for app in $apps; do
     app_name=${app/apps\//}
     app_name=${app_name::-1}
+
+    printf "Building docker image: $app_name\n"
 
     git fetch --tags
     last_image_tag=$(git tag --sort=-version:refname | grep -P "^$app_name@\d+.\d+.\d+$" | head -n 1)
@@ -25,10 +27,16 @@ if (( ${#apps[@]} )); then
       new_app_version=$(semver bump patch "$last_version")
     fi
 
+    printf "New image version: $new_app_version\n"
+
     github_tag="$app_name@$new_app_version"
     image_tag="$APP_DOCKER_REGISTRY/$app_name:$new_app_version"
 
+    printf "New image tag: $github_tab\n"
+    printf "New image: $image_tag\n"
+
     docker build -t $image_tag --target dev $app
+
 
     git tag -a $github_tag -m "my version 1.4"
 
@@ -37,6 +45,7 @@ if (( ${#apps[@]} )); then
     docker login -u "crkushal7" -p "d45444eb-d5d9-4025-a482-d7482517e55a"
 
     docker push $image_tag
+    printf "Successfully build docker images...\n"
   done
 else
   printf "Skipping no changes detected...\n"
--- tag.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git fetch --tags
-last_image_tag=$(git tag --sort=-version:refname | grep -P "^$app_name@\d+.\d+.\d+$" | head -n 1)
-
-printf "$last_image_tag"
