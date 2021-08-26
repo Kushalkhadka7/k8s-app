6f61b739a8fa42a7f7ae41f42f925f94501bb1bd
test <test@test.com>
2021-08-26 22:06:46 +0545

    Update docker build script

--- build.sh
+++ build.sh
@@ -7,11 +7,13 @@ apps=$(git diff --name-only | sort -u | grep -oP "apps\/.+?\/" | cat | uniq)
 APP_DOCKER_REGISTRY="crkushal7"
 
 if (( ${#apps[@]} != 0 )); then
-  printf "Publishing images...\n"
+  printf "Building docker images...\n"
 
   for app in $apps; do
     app_name=${app/apps\//}
     app_name=${app_name::-1}
+
+    printf "Building docker image: $app_name\n"
 
     git fetch --tags
     last_image_tag=$(git tag --sort=-version:refname | grep -P "^$app_name@\d+.\d+.\d+$" | head -n 1)
@@ -25,10 +27,17 @@ if (( ${#apps[@]} != 0 )); then
       new_app_version=$(semver bump patch "$last_version")
     fi
 
+    printf "New image version: $new_app_version\n"
+
     github_tag="$app_name@$new_app_version"
     image_tag="$APP_DOCKER_REGISTRY/$app_name:$new_app_version"
 
+    printf "New image tag: $github_tab\n"
+    printf "New image: $image_tag\n"
+
     docker build -t $image_tag --target dev $app
+
+    printf "Successfully build docker images...\n"
   done
 else
   printf "Skipping no changes detected...\n"
